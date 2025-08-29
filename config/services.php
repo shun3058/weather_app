44288d04980256811263afafa668d9b3374f513a
shun3058 <greeeen.1004.1004@gmail.com>
2025-08-29 19:52:04 +0900

    openweatherの情報を登録

--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         ],
     ],
 
+    'openweather' => [
+        'api_key' => env('OPENWEATHER_API_KEY'),
+        'base_url' => env('OPENWEATHER_BASE_URL', 'https://api.openweathermap.org/data/2.5')
+    ],
+
 ];
