3443c67b5bb82ea6be948628d2d989ab0277cc0d
shun3058 <greeeen.1004.1004@gmail.com>
2025-08-30 16:45:13 +0900

    api接続

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\City;
+use App\Models\WeatherData;
+use App\Services\WeatherService;
 
 class WeatherController extends Controller
 {   
+    private WeatherService $weatherService;
+
+    public function __construct(WeatherService $weatherService)
+    {
+        $this->weatherService = $weatherService;
+    }
+    
     public function index()
     {
-        return view('weather.index');
+        return view('weather.index', [
+            'apiConfigured' => $this->weatherService->isApiConfigured(s)
+        ]);
     }
 
     public function search(Request $request)
@@ -20,9 +31,39 @@ class WeatherController extends Controller
 
         $cityName = $request->input('city');
 
+        if (!$this->weatherService->isApiConfigured()) {
+            return back()->withErrors(['city' => 'API設定が間違っています。']);
+        }
+
+        $weatherData = $this->weatherService->getCurrentWeather($cityName);
+
+        if (!$weatherData) {
+            return back()->withErrors(['city' => '都市がみつからないか、APIエラーが発生しました。']);
+        }
+
+        $city = City::firstOrCreate([
+            'name' => $weatherData['city_name'],
+            'country_code' => $weatherData['country_code'],
+        ], [
+            'latitude' => $weatherData['latitude'],
+            'longtitude' => $weatherData['longtitude'],
+        ]);
+
+        $weather = WeatherData::create([
+            'city_id' => $city->id,
+            'provider' => $weatherData['provider'],
+            'temperature' => $weatherData['temperature'],
+            'humidity' => $weatherData['humidity'],
+            'pressure' => $weatherData['pressure'],
+            'description' => $weatherData['description'],
+            'icon' => $weatherData['icon'],
+            'wind_speed' => $weatherData['wind_speed'],
+            'forecast_date' => $weatherData['forecast_date'],
+        ]);
+
         return view('weather.result', [
-            'cityName' => $cityName,
-            'message' => 'API連携は今後実装します'
+            'city' => $city,
+            'weatherData' => [$weather],
         ]);
     }
 }
